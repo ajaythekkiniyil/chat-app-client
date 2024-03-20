3a25ae4456191b0efaf9634a2864846cecb33da6
ajay <ajaythekkiniyi@gmail.com>
2024-03-20 18:47:41 +0530

    redux persistor added

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './store/store.js'
+
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit"
 import themeSlice from './themeSlice'
 import chatSlice from './chatSlice'
 import currentConversationSlice from './currentConversationSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    theme: themeSlice,
+    chatOpen: chatSlice,
+    currentConversation: currentConversationSlice,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        theme: themeSlice,
-        chatOpen: chatSlice,
-        currentConversation: currentConversationSlice,
-    },
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // Ignore these action types
+                ignoredActions: ['persist/PERSIST']
+            },
+        })
 })
+
+export const persistor = persistStore(store)
